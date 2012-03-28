07d47a71e5ac88bde5f224b7c9d52a5491bc428e
Daisuke Maki <dmaki@cpan.org>
2012-03-28 04:18:51 +0000

    Import of DMAKI/AnyEvent-HTTP-MXHR-0.00007 from CPAN.
    gitpan-cpan-distribution: AnyEvent-HTTP-MXHR
    gitpan-cpan-version:      0.00007
    gitpan-cpan-path:         DMAKI/AnyEvent-HTTP-MXHR-0.00007.tar.gz
    gitpan-cpan-author:       DMAKI
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/HTTP/MXHR.pm
+++ lib/AnyEvent/HTTP/MXHR.pm
@@ -1,10 +1,11 @@
 package AnyEvent::HTTP::MXHR;
 use strict;
+use AnyEvent '6.01';
 use AnyEvent::HTTP;
 use AnyEvent::Util qw(guard);
 use base qw(Exporter);
 our @EXPORT = qw(mxhr_get);
-our $VERSION = '0.00006';
+our $VERSION = '0.00007';
 
 sub mxhr_get ($@) {
     my $cb = pop;
@@ -40,6 +41,8 @@ sub mxhr_get ($@) {
                 $on_error->("Connection failed") if $on_error;
                 return ();
             }
+
+            $state{handle} = $handle;
 
             my $callback; $callback = sub {
                 my ($handle, $data) = @_;
